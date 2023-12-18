7ad67d24da57f4df30238618adb8abdbaacc0a2a
Umar Hasan <sansanbgl@gmail.com>
2023-12-18 19:51:28 +0700

    Update

---
+++ E.py
@@ -0,0 +1,8 @@
+E. coli                           2466
+Fluoride                          1090
+Lead                               314
+Lead - Total                      1075
+Manganese                          782
+Residual Free Chlorine             813
+Residual Monochloramine            168
+Residual Total Chlorine            902
---
+++ Highest copper value for each year, regi.py
@@ -0,0 +1,13 @@
+Highest copper value for each year, region, and system (sorted):
+Year    region              system        health_compliance
+2021.0  Northern            Cadell        1.0                  2.0000
+2020.0  Remote Communities  Oak Valley    1.0                  1.9860
+2021.0  Remote Communities  Oak Valley    1.0                  1.9860
+2020.0  South East          Naracoorte    1.0                  1.4910
+        Remote Communities  Nepabunna     1.0                  1.4520
+                                                                ...  
+2022.0  Northern            Woolpunda     1.0                  0.0014
+2023.0  Northern            Woolpunda     1.0                  0.0013
+2021.0  Remote Communities  Kalka         1.0                  0.0007
+2022.0  Remote Communities  Kalka         1.0                  0.0007
+2023.0  Northern            Leigh Creek   1.0                  0.0006
